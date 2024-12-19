4c17e86e8e0b1254ac45b988d3524124e75cb240
Anton Kovalchuk <koval400k@gmail.com>
2024-12-19 01:06:42 +0100

    add docker

--- initialize.sh
+++ initialize.sh
@@ -4,18 +4,6 @@ set -e
 echo 'The initialize script is about to start. It will install the dependencies as well as fetch submodules repositories and do all the necessary preliminary work'
 sleep 5
 
-source ./get_env_var.sh
-
-export L1_CHAIN_ID="$(get_env_var "L1_CHAIN_ID")"
-l1_host_value="$(get_env_var "L1_BLOCK_EXPLORER_API_HOST")"
-export L1_BLOCK_EXPLORER_API_URL="https://${l1_host_value}/api"
-export L1_BLOCK_EXPLORER_BROWSER_URL="$(get_env_var "L1_BLOCK_EXPLORER_BROWSER_URL")"
-
-export L2_CHAIN_ID="$(get_env_var "L2_CHAIN_ID")"
-l2_host_value="$(get_env_var "L2_BLOCK_EXPLORER_API_HOST")"
-export L2_BLOCK_EXPLORER_API_URL="https://${l2_host_value}/api"
-export L2_BLOCK_EXPLORER_BROWSER_URL="$(get_env_var "L2_BLOCK_EXPLORER_BROWSER_URL")"
-
 yarn
 
 git submodule init
